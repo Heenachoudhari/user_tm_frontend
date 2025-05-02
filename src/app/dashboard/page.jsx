244353c145e1d1a3106aef66eb3e80308694ae45
prashu-tech <prashantparsane894@gmail.com>
2025-05-02 12:54:34 +0530

    dashboard by Prashant

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,14 +1,23 @@
+'use client';
+
+import React, { useState } from "react";
 import OverviewHeader from "@/Component/dashboard/overview";
+import TaskCompletedChart from "@/Component/dashboard/taskcompletedchart";
 import NavBar from "@/Component/Navbar/navbar";
 import Sidebar from "@/Component/Usersidebar/usersidebar";
-import React from "react";
+import ProgressChart from "@/Component/dashboard/ProgressChart";
+import AttendanceChart from "@/Component/dashboard/attendancechart";
+import PerformanceSummary from "@/Component/dashboard/performancesummary";
+import ProjectStatusChart from "@/Component/dashboard/projectstatuschart";
+import PerformanceChart from "@/Component/dashboard/performancechart";
 
-function page() {
+function Page() {
+  const [selected, setSelected] = useState("This Year"); 
+
   return (
     <div className="min-h-screen md:flex bg-white">
-
       {/* Desktop Sidebar Section (visible on md+) */}
-      <div className="md:w-1/6 ">
+      <div className="md:w-1/6">
         <Sidebar />
       </div>
 
@@ -18,21 +27,41 @@ function page() {
         <NavBar />
 
         {/* Page Content */}
-        <main className="hidden md:block">
-            <OverviewHeader />
-        </main>
+        <main className="hidden md:block px-6 py-6">
+  <OverviewHeader selected={selected} setSelected={setSelected} />
+
+  <div className="grid grid-cols-3 gap-6 mt-6">
+    
+      <TaskCompletedChart selected={selected} />
+ 
+
+      <ProgressChart selected={selected} />
 
 
+   
+      <AttendanceChart selected={selected} />
+    
+
+
+      <PerformanceSummary selected={selected} />
+   
+
+    
+      <ProjectStatusChart selected={selected} />
+   
+    
+      <PerformanceChart timeframe={selected} />
+   
+  </div>
+</main>
 
 
 
         {/* Mobile View */}
         <main className="block md:hidden"></main>
-
       </div>
-
     </div>
   );
 }
 
-export default page;
+export default Page;
